484f2f06f1c39c1c39fa89fcefbf4cbcbd5a47ef
Arnau Saborit <arnausv@uoc.edu>
2024-05-14 17:58:03 +0200

    PWA done. Waiting to review.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgOptimizedImage } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, isDevMode } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   BrowserModule,
@@ -21,6 +21,7 @@ import { MainLayoutComponent } from './core/main-layout/main-layout.component';
 import { WelcomePageComponent } from './routes/pages/welcome-page/welcome-page.component';
 import { SharedModule } from './shared.module';
 import { PopcornSpinnerComponent } from './shared/components/popcorn-spinner/popcorn-spinner.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 register();
 
 @NgModule({
@@ -45,6 +46,12 @@ register();
     FontAwesomeModule,
     SharedModule,
     ToastrModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
