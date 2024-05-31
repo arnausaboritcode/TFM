0ba31833c70b427579821064432b6041310d938a
Arnau Saborit <arnausv@uoc.edu>
2024-05-31 20:27:26 +0200

    Bbd working on google cloud

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BASE_API_URL_BACKEND: 'https://tfm-az25.vercel.app/api',
+  BASE_API_URL_BACKEND: 'http://localhost:8000/api',
   API_KEY_FRONTEND: '7fafb556274856965adfad6dc6852cb7',
   BASE_API_URL_FRONTEND: 'https://api.themoviedb.org/3/',
 };
