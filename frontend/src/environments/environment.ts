991b796f2a41d96188b2acc99e07842b2d4e4fc0
Arnau Saborit <arnausv@uoc.edu>
2024-06-03 17:03:42 +0200

    Frontend environments updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BASE_API_URL_BACKEND: 'https://popcorn-backend-flame.vercel.app/api',
+  BASE_API_URL_BACKEND: 'https://popcorn-67a85095e746.herokuapp.com/api',
   API_KEY_FRONTEND: '7fafb556274856965adfad6dc6852cb7',
   BASE_API_URL_FRONTEND: 'https://api.themoviedb.org/3/',
 };
