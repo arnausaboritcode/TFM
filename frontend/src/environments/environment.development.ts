1d7f4c5d6f9d5267a017d5021532fc985df409ca
Arnau Saborit <arnausv@uoc.edu>
2024-06-03 17:20:16 +0200

    Environment variables updated

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  BASE_API_URL_BACKEND: 'https://popcorn-67a85095e746.herokuapp.com/api',
+  BASE_API_URL_BACKEND: 'https://popcornbackend-4d7d77b31569.herokuapp.com/api',
   API_KEY_FRONTEND: '7fafb556274856965adfad6dc6852cb7',
   BASE_API_URL_FRONTEND: 'https://api.themoviedb.org/3/',
 };
